b5cdd59723abdeb3668fea9ab06efb0d8aab810d
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-08-28 22:39:19 +0500

    main in progress

--- IntroductionOOP/Fraction/main.cpp
+++ IntroductionOOP/Fraction/main.cpp
@@ -1,7 +1,10 @@
 ﻿#include<iostream>
 using namespace std;
 
-class Fraction
+class Fraction;        //Объявление класса
+Fraction operator*(Fraction left, Fraction right);//объявление функции
+Fraction operator/(const Fraction& left, const Fraction& right);
+class Fraction         //Реализация класса
 {
 	int integer;       // Целая часть
 	int numerator;     // Числитель
@@ -15,7 +18,7 @@ public:
 	{
 		return numerator;
 	}
-	int get_denomirator()const
+	int get_denominator()const
 	{
 		return denominator;
 	}
@@ -94,6 +97,40 @@ public:
 		return *this;
 	}
 
+	Fraction& operator*=(const Fraction& other)
+	{
+		return *this = *this * other;
+	}
+
+	Fraction& operator/=(const Fraction& other)
+	{
+		return *this = *this / other;
+	}
+
+	//				Incremento/Decremento:
+	Fraction& operator++()
+	{
+		integer++;
+		return *this;
+	}
+	const Fraction operator++(int)
+	{
+		Fraction old = *this;
+		integer++;
+		return old;
+	}
+	Fraction& operator--()
+	{
+		integer--;
+		return *this;
+	}
+	const Fraction operator--(int)
+	{
+		Fraction old = *this;
+		integer--;
+		return old;
+	}
+
 	//Methods:
 	Fraction& to_improper()
 	{
@@ -107,6 +144,33 @@ public:
 		//перевод в правильную дробь:
 		integer += numerator / denominator;
 		numerator %= denominator;
+		return *this;
+	}
+	Fraction inverted()const
+	{
+		Fraction inverted = *this;
+		inverted.to_improper();
+		swap(inverted.numerator, inverted.denominator);	//Функция swap() меняет местами 2 переменные
+		return inverted;
+	}
+	Fraction& reduce()
+	{
+		//https://www.webmath.ru/poleznoe/formules_12_7.php
+		int more, less, rest;
+		//more - больше
+		//less - меньше
+		//rest - остаток
+		if (numerator > denominator)more = numerator, less = denominator;
+		else	less = numerator, more = denominator;
+		do
+		{
+			rest = more % less;
+			more = less;
+			less = rest;
+		} while (rest);
+		int GCD = more;	//GCD - Greatest Common Divisor (Наибольший общий делитель)
+		numerator /= GCD;
+		denominator /= GCD;
 		return *this;
 	}
 	void Print()const
@@ -123,21 +187,37 @@ public:
 	}
 };
 
+Fraction operator+(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return Fraction
+	(
+		left.get_numerator() * right.get_denominator() + right.get_numerator() * left.get_denominator(),
+		left.get_denominator() * right.get_denominator()
+	).to_proper();
+}
+
 Fraction operator*(Fraction left, Fraction right)
 {
 	left.to_improper();
 	right.to_improper();
 	/*Fraction result;
 	result.set_numerator(left.get_numerator()* right.get_numerator());
-	result.set_denominator(left.get_denomirator()* right.get_denomirator());*/
+	result.set_denominator(left.get_denominator()* right.get_denominator());*/
 	return Fraction
 	(
 		left.get_numerator() * right.get_numerator(),
-		left.get_denomirator() * right.get_denomirator()
-	).to_proper();
+		left.get_denominator() * right.get_denominator()
+	).to_proper().reduce();
 }
-
+Fraction operator/(const Fraction& left, const Fraction& right)
+{
+	return left * right.inverted();
+}
 //#define CONSTRUCTOR_CHECK
+#define ARITHMETICAL_OPERATORS_CHECK
+//#define INCREMENTO_DECREMENTO_CHECK
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -156,12 +236,37 @@ void main()
 
 #endif // CONSTRUCTOR_CHECK
 
-	Fraction A(1, 2, 3);
+#ifdef ARITHMETICAL_OPERATORS_CHECK
+	Fraction A(2, 3, 4);
 	A.Print();
 
-	Fraction B(2, 3, 4);
+	Fraction B(3, 4, 5);
 	B.Print();
 
 	Fraction C = A * B;
 	C.Print();
+
+	C = A / B;
+	C.Print();
+
+	A *= B;
+	A.Print();
+
+	A /= B;
+	A.Print();
+
+	C = A + B;
+	C.Print();
+
+#endif // ARITHMETICAL_OPERATORS_CHECK
+
+#ifdef INCREMENTO_DECREMENTO_CHECK
+	Fraction A(2, 3, 4);
+	Fraction B(2, 3, 4);
+	B *= A++;
+	A.Print();
+	B.Print();
+#endif // INCREMENTO_DECREMENTO_CHECK
+
+
 }
